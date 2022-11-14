59c761cb3bf56084736da8407d33fcd7bd7d34a3
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2022-11-14 17:26:58 +0200

    first commit

---
+++ lesson2/script/script.js
@@ -0,0 +1,27 @@
+const userName = "Oksana";
+    console.log("userName", userName);
+
+const userSurname = "Prokopova";
+    console.log("userSurname", userSurname);
+
+const userAge = "44";
+    console.log("userAge", userAge);
+
+const userCity = "Odessa";
+ 
+
+
+    
+
+let userNew;
+let listShopping;
+let userOnline;
+let userSalary;
+let cursorCoord;
+
+
+
+let greetings;
+    greetings = "Hello, World!";
+    console.log(greetings);
+    
