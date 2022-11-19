1699e2a471e235a49c6e934b33a84dab7cd14434
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2022-11-19 19:29:59 +0200

    change file lesson3

--- lesson3/script/script.js
+++ lesson3/script/script.js
@@ -1,23 +1,23 @@
 // 1)
 
-const a = 5;
-const b = 10;
-let additionResult;
-let subtractionResult;
-let divisionResult;
-let miltiplicationResult;
+// const a = 5;
+// const b = 10;
+// let additionResult;
+// let subtractionResult;
+// let divisionResult;
+// let miltiplicationResult;
 
-additionResult = a + b;
-    console.log("a + b = ", additionResult);
+// additionResult = a + b;
+//     console.log("a + b = ", additionResult);
 
-subtractionResult = a - b;
-    console.log("a - b = ", subtractionResult);
+// subtractionResult = a - b;
+//     console.log("a - b = ", subtractionResult);
 
-divisionResult = a / b;
-    console.log("a / b = ", divisionResult);
+// divisionResult = a / b;
+//     console.log("a / b = ", divisionResult);
 
-miltiplicationResult = a * b;
-    console.log("a * b = ", miltiplicationResult);
+// miltiplicationResult = a * b;
+//     console.log("a * b = ", miltiplicationResult);
 
 
 
@@ -40,13 +40,67 @@ miltiplicationResult = a * b;
 
 // 3)
 
-let number1 = Number(prompt("Enter the first number"));
-let number2 = Number(prompt("Enter the second number"));
+// let number1 = Number(prompt("Enter the first number"));
+// let number2 = Number(prompt("Enter the second number"));
 
-if (number1 > number2){
-    alert( number1 + " more than " + number2);
-}else if( number1 < number2){
-    alert(number1 + " less than " + number2);
+// if (number1 > number2){
+//     alert( number1 + " more than " + number2);
+// }else if( number1 < number2){
+//     alert(number1 + " less than " + number2);
+// }else{
+//     alert(number1 + " equal " + number2);
+// }
+
+
+
+// 4)
+
+// let num1 = Number(prompt("Enter the first number"));
+// let num2 = Number(prompt("Enter the second number"));
+// let action = (prompt("Enter the desired action( +, -, *, /)"));
+
+// const sumResult = num1 + num2;
+// let differenceResult = num1 - num2;
+// let miltiplicationResult = num1 * num2;
+// let divisionResult = num1 / num2;
+
+// switch(action){
+//     case "+": 
+//         alert(" Result = " + sumResult);
+//         break;
+//     case "-":
+//         alert(" Result = " + differenceResult);
+//         break;
+//     case "*":
+//         alert(" Result = " + miltiplicationResult);
+//         break;
+//     default:
+//         alert(" Result = " + divisionResult);
+// }
+
+
+
+// 5)
+
+const MIN_AGE = 12;
+const minMaxAge = 18;
+const MAX_AGE = 80;
+const maxMinAge = 60;
+const visitorAge = Number(prompt("How old are you?"));
+let permissionMinAge;
+let permissionMaxAge;
+
+
+if(visitorAge >= MIN_AGE && visitorAge < minMaxAge){
+    permissionMinAge = confirm("Do you have permission from your parents?");
+    if(permissionMinAge) {alert("Well, entry is allowed!");}
+    else{alert("Sorry, you are not allowed to enter!")}
+}else if( visitorAge >= maxMinAge && visitorAge < MAX_AGE){
+    permissionMaxAge = confirm("You have a companion under 60 years age?");
+    if(permissionMaxAge){alert("If you are sure entry is a allowed.");}
+    else{alert("Sorry, you are not allowed to enter!")}
+}else if( visitorAge < MIN_AGE || visitorAge > MAX_AGE){
+    alert("Sorry, you are not allowed to enter!");
 }else{
-    alert(number1 + " equal " + number2);
+    alert("We`re glad to see you, entry allowed!")
 }
