1354ceb29708ec403df0b8818eb077d64fb617e3
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2022-11-25 21:24:25 +0200

    creation file lesson5

---
+++ lesson5/script/script.js
@@ -0,0 +1,60 @@
+// 1)
+
+// for(let i = 1 ; i <=10 ; i++) {
+//     console.log(`i`, i);
+// }
+
+// for(let j = 20 ; j>= 10 ; j--) {
+//     console.log(`j`, j);
+// }
+
+
+// let k = 1;
+// while( k <= 10 ) {
+//     console.log(`k`, k);
+//     k++;
+// }
+
+
+// let l =20;
+// while( l >=10) {
+//     console.log(`l`, l);
+//     l--;
+// }
+
+
+// let a = 1;
+// do {
+//     console.log(`a`, a);
+//     a++;
+// }while ( a  <= 10 );
+
+// let b = 20;
+// do {
+//     console.log(`b`, b);
+//     b--;
+// }while( b >= 10 );
+
+
+
+
+// 2)
+let i;
+let j;
+
+const naturalNumbers = (start, finish) => {
+    for(let i = start ; i <= finish; i++){
+       
+        let j = i;
+        while( i % J == 0){
+            console.log( i);console.log( i);
+        }
+        // for(let j = start; j < i ; j++){
+        //     if( i % j == 0){
+        //         continue;
+        //     }
+        // }
+        
+C};
+
+naturalNumbers(1, 10);
