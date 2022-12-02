bbe3ead70a4609c8bed2dd16d50109025aba6026
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2022-12-02 20:35:59 +0200

    change file lesson6

--- lesson6/script/script.js
+++ lesson6/script/script.js
@@ -7,43 +7,61 @@
 // После валидации данных необходимо отформатировать имя и фамилию и вывести на экран с помощью alert полное имя пользователся в правильном формате. Под форматированием имеется ввиду написание имени и фамилии с большой буквы. Например, если юзер ввел jOhn bROWN, то в результате форматирования должно получится John Brown.
 // Hints: вам может понадобится цикл do while, так же советуем разбивать код на отдельные функции, каждая с ОДНОЙ логической целью.
 
-
+let userName;
+let userSurname;
+let userPassword;
  
 
  const authorise = () => {
 
-    let userName;
-    let userSurname;
-    let userPassword;
+ 
+
+      
+
+      do{
+
+         userName = prompt("Enter your name: ");
+         userSurname = prompt("Enter your surname: ");
+
+         firstLetterName = userName.slice(0, 1);
+         lastLetterName = userName.slice(1);
+         firstLetterSurname = userSurname.slice(0, 1);
+         lastLetterSurname = userSurname.slice(1);
+
+         
+ 
+ 
+ 
+         if(userName.length >= 1 && userSurname.length >= 1){
+            alert( firstLetterName.toUpperCase + lastLetterName.toLowerCase + " " + firstLetterSurname.toUpperCase + lastLetterSurname.toLowerCase );
+            break;
+         }else{
+            alert("Enter correct data!");
+            continue;
+         };
+            
 
 
+         userPassword = prompt("Enter your password: ");
 
-    do{
+         bigLetters = userPassword[userPassword.length].toUpperCase();
+         littleLetters = userPassword[userPassword.length].toLowerCase();
+         numberLetters;
 
-        userName = prompt("Enter your name: ");
-        userSurname = prompt("Enter your surname: ");
+         
 
-        let firstLetterName = userName.slice(0, 1);
-        let firstLetterSurname= userSurname.slice(0, 1);
-        let lastLetterName = userName.slice(1);
-        let lastLetterSurname = userSurname.slice(1);
-        let lastName = `${firstLetterName.toUpperCase}${lastLetterName.toLowerCase} ${firstLetterSurname}${lastLetterSurname}`;
+         if(userPassword.length >= 6 && userPassword == bigLetters && userPassword == littleLetters && isFinite(userPassword)){
+            break;
+         }else{
+            alert("Enter correct password! (AbCxx12, 1234xY, abcdeF)");
+            continue;
+         }
+
+      }while(true);
+
+      alert("Welcome!");
 
 
-
-        if(userName.length >= 1 && userSurname.length >= 1){
-           alert(lastName);
-        }else{
-           alert("Enter correct data!");
-        };
-
-        userPassword = prompt("Enter your password: ");
-        if(userPassword.length >= 6 ){
-
-        }
-
-    }
-
- };
+};
 
 authorise();
