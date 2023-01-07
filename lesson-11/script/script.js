aa584697e0834f47734f886847f3e19dad0c8747
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2023-01-07 17:34:34 +0200

    change files lesson-11 - lesson-14

---
+++ lesson-11/script/script.js
@@ -0,0 +1,27 @@
+// Реализовать функцию, которая принимает на вход массив с данными (товары) и рендерит на страницу карточки товаров.
+
+// Пример масисива данных, с которыми должна работать функция, используйте их для теста:
+
+// const products = [
+//   {
+//     name: "Iphone 12",
+//     brand: "Apple",
+//     price: 3200000,
+//     properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+//   },
+//   {
+//     name: "Galaxy S20 Ultra",
+//     brand: "Samsung",
+//     price: 2900000,
+//     properties: ["120 hz screen", "Water resistance"],
+//   },
+//   {
+//     name: "MI 9",
+//     brand: "Xiaomi",
+//     price: 1300000,
+//     properties: ["Best price", "Pay less - get more!"],
+//   },
+// ];
+
+
+// Для реазизации задачи используйте разные способы, как и innerHTML с шаблонными строками, так и метод append()
