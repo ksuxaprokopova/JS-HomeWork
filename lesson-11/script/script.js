4f51a55ea61ff378970566e53ce436aec32e7b48
ksuxaprokopova <ksuxaprokopova0606@gmail.com>
2023-01-09 20:25:33 +0200

    change files lesson-11

--- lesson-11/script/script.js
+++ lesson-11/script/script.js
@@ -2,26 +2,81 @@
 
 // Пример масисива данных, с которыми должна работать функция, используйте их для теста:
 
-// const products = [
-//   {
-//     name: "Iphone 12",
-//     brand: "Apple",
-//     price: 3200000,
-//     properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
-//   },
-//   {
-//     name: "Galaxy S20 Ultra",
-//     brand: "Samsung",
-//     price: 2900000,
-//     properties: ["120 hz screen", "Water resistance"],
-//   },
-//   {
-//     name: "MI 9",
-//     brand: "Xiaomi",
-//     price: 1300000,
-//     properties: ["Best price", "Pay less - get more!"],
-//   },
-// ];
+const products = [
+  {
+    name: "Iphone 12",
+    brand: "Apple",
+    price: 3200000,
+    properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+  },
+  {
+    name: "Galaxy S20 Ultra",
+    brand: "Samsung",
+    price: 2900000,
+    properties: ["120 hz screen", "Water resistance"],
+  },
+  {
+    name: "MI 9",
+    brand: "Xiaomi",
+    price: 1300000,
+    properties: ["Best price", "Pay less - get more!"],
+  },
+];
 
 
 // Для реазизации задачи используйте разные способы, как и innerHTML с шаблонными строками, так и метод append()
+
+const createCard = () => {
+  for(i = 0; i <= products.length; i++){
+
+    const createProductCard = (name, brand, properties, price) => {
+      const cardContainer = document.createElement("div");
+      const productName = document.createElement("h1");
+      const productBrand = document.createElement("h2");
+      const productPrise = document.createElement("h2");
+      
+    
+    
+      productName.innerText = name;
+      productBrand.innerText = brand;
+
+    
+      const productProperties = (properties) => {
+        const listProperties = document.createElement("ul");
+       
+    
+        properties.forEach((item) => {
+          const itemProperties = document.createElement("li");
+          itemProperties.innerText = item;
+    
+          listProperties.append(itemProperties);
+        });
+    
+        document.body.append(listProperties);
+      }
+    
+      productProperties(products);
+
+
+      productPrise.innerText = price;
+
+      
+    
+      cardContainer.append(productBrand, productName, productProperties, productPrise);
+    
+      console.dir(cardContainer);
+      console.log(cardContainer)
+    
+      return cardContainer;
+    
+    
+    }
+    
+    const cards = createProductCard(products);
+    console.log(cards)
+    
+    document.body.append(cards);
+  }
+}
+const result = createCard(products)
+
